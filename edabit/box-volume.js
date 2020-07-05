529ab5ef3333a3903b7fbbe90f014b40564fa6b8
anhtungbui <anhtung.bui@gmail.com>
2020-07-06 00:47:17 +0200

    Uploaded

---
+++ edabit/box-volume.js
@@ -0,0 +1,23 @@
+/*
+Volume of a Box
+source: edabit - very easy
+https://edabit.com/challenge/ms3q5GYSpFpwxeFWX
+
+Create a function that takes an object argument sizes (contains width, length, height keys) and returns the volume of the box.
+
+** Examples
+volumeOfBox({ width: 2, length: 5, height: 1 }) ➞ 10
+
+volumeOfBox({ width: 4, length: 2, height: 2 }) ➞ 16
+
+volumeOfBox({ width: 2, length: 3, height: 5 }) ➞ 30
+
+** Notes
+Don't forget to return the result.
+Remember that the values are in an object.
+Volume is length * width * height.
+*/
+
+// TEST CASES
+console.log(volumeOfBox({ width: 2, length: 5, height: 1 }));
+console.log(volumeOfBox({ width: 4, length: 2, height: 2 }));
